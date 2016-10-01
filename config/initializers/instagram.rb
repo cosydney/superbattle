5fc3c33b314811016b016062ca5c012e2716056b
Sydney Cohen <chnsydney@gmail.com>
2016-10-01 10:09:11 +0200

    testing instagram api

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -2,4 +2,5 @@
     Instagram.configure do |config|
     config.client_id = ENV["INSTA_CLIENT_ID"]
     config.access_token = ENV["INSTA_ACCESS_TOKEN"]
+    config.client_secret = ENV['INSTA_CLIENT_SECRET']
  end
