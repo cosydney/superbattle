fe2ae13c0ea352b4cfdf3a57284ad45770f5a639
Sydney Cohen <chnsydney@gmail.com>
2016-10-28 17:00:14 +0200

    error handling capybara

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,12 +6,17 @@ class PagesController < ApplicationController
   def stat
     if params[:superuser].nil? || params[:superuser].empty? ||
       params[:usercmp].nil? || params[:usercmp].empty?
-      redirect_to root_path
+      redirect_to root_path, notice: "Username cannot be empty"
     else
       username = params[:superuser]
       usernamecmp = params[:usercmp]
-      @superuser = Superuser.new(username)
-      @usercmp = Superuser.new(usernamecmp)
+      begin
+        @superuser = Superuser.new(username)
+        @usercmp = Superuser.new(usernamecmp)
+      rescue Capybara::ElementNotFound
+        redirect_to root_path, alert: "Username not found"
+      end
+
     end
   end
 end
